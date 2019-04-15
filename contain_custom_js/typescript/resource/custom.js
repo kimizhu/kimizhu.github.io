b3962b7bac7eb44f31d7bdb7d111bb63a47fb4f4
Qinghui Meng <v-qimen@mircosoft.com>
2019-04-15 17:35:17 +0800

    add custom js

---
+++ contain_custom_js/typescript/resource/custom.js
@@ -0,0 +1,8 @@
+<script>
+setTimeout(function() {
+    var elems = document.getElementsByClassName('img-box-shadow')
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].style = "box-shadow: 10px 10px 20px 5px #888888; margin: 0px 15px;"
+    }
+}, 0);
+</script>
