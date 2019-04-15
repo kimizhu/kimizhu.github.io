6d43895813288f2abe758fb12c1b8e5bb31a8a1a
Qinghui Meng <v-qimen@mircosoft.com>
2019-04-15 17:50:01 +0800

    clean

--- ie_html/resource/replace.js
+++
@@ -1,8 +0,0 @@
-$("#page").prepend("<div id='archived_warning'>This blog has been archived!</div>");
-var content = $("#content").html();
-if(content !='')
-{ 
-	content ='<p>'+ content.replace(/\n/g,'</p> <p>').replace(/\r\n/g,'</p> <p>' )+'</p>'; 
-	$("#content").html(content);
-}
-var page = $("#page").html();
